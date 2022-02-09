74d94063faea0d011d9123b11409b55d9a99f8ff
Hilmar <hilmare@me.com>
2022-02-09 14:29:13 +0100

    Remove unnecessary import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import EditList from './edit/EditList.js';
+// import EditList from './edit/EditList.js';
 ReactDOM.render(
   <React.StrictMode>
     <App />
