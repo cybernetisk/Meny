336381a59332f3bb112b477b05726352facb046d
Hilmar <hilmare@me.com>
2022-02-15 16:13:19 +0100

    Add comment with tips about terminal utility

--- src/json2csv.py
+++ src/json2csv.py
@@ -1,5 +1,5 @@
 import json  
-
+#use this instead: miller
 data = json.load(open("./data.json"))
 csv = open("data.csv", 'w')
 csv.write("Navn;Egendefinert enhet (maks 4 tegn);MVA.;Variant1 navn;Variant1 variant;Variant2 navn;Variant2 variant;Variant3 navn;Variant3 variant;SKU;Pris;Innkjøpspris;Strekkode;På lager;Variant-ID;Produkt-ID;ID (Ikke rediger): [n-un-vat-vo0n-vo0v-vo1n-vo1v-vo2n-vo2v-sku-p-cp-bc-ib-vid-id-k]\n")
