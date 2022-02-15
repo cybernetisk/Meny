7d078ec4df26d9b1c6bd142a36498c3ecc3738e5
Hilmar <hilmare@me.com>
2022-02-15 16:15:53 +0100

    Fix MVA bug

--- src/json2csv.py
+++ src/json2csv.py
@@ -4,7 +4,7 @@ data = json.load(open("./data.json"))
 csv = open("data.csv", 'w')
 csv.write("Navn;Egendefinert enhet (maks 4 tegn);MVA.;Variant1 navn;Variant1 variant;Variant2 navn;Variant2 variant;Variant3 navn;Variant3 variant;SKU;Pris;Innkjøpspris;Strekkode;På lager;Variant-ID;Produkt-ID;ID (Ikke rediger): [n-un-vat-vo0n-vo0v-vo1n-vo1v-vo2n-vo2v-sku-p-cp-bc-ib-vid-id-k]\n")
 for d in data:
-    t= d['name']=='Alkoholfritt'
+    t= d['name']=='Snacks'
     for p in d['products']:
         f = p['type'] != "flaske"
         print(p)
